b15cd8391ac68069feb1d819d4e4c400a11c4e86
Ninja <esomina@gmail.com>
2016-12-01 16:20:55 +0300

    1

--- OLOG/PageRegions/Admin/SearchAjax.php
+++ OLOG/PageRegions/Admin/SearchAjax.php
@@ -17,20 +17,20 @@ class SearchAjax implements InterfaceAction {
     public function action() {
         $query = POSTAccess::getOptionalPostValue(self::SEARCH_FIELD);
         $ids_arr =  Block::getIdsArrForSearchQuery($query);
-
+        $query = preg_quote($query);
         $content_html = '';
         foreach ($ids_arr as $id) {
             $block = Block::factory($id);
             $action = (new BlockEditAction($id))->url();
 
-            $query = preg_quote($query);
+
 
             $body = '';
             if (preg_match("#(.{0,100}" . $query . ".{0,100})#im",$block->getBody(), $p)) {
                 $body = $p[1];
             }
 
-            $content_html .= "<li><a href='".  $action ."'>".$block->getInfo()."</a> <div style='color:#CCC; font-size:10px'>" . $body ."</div> </li>";
+            $content_html .= "<li><a href='".  $action ."'>".$block->getInfo()."</a> " . " <div style='color:#CCC; font-size:10px'>" . $body ."</div> </li>";
         }
 
         $content = ['success' => true, 'html' => $content_html];
